7e38964aa190e1c09d8d9833c5340b241e998eaf
bonnesbaby14 <bonnesbaby14@gmail.com>
2022-05-24 22:26:23 -0500

    dashboards

---
+++ dashboardAdmin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li>
+            <a href="#">Maestros</a>
+        </li>
+        <li>
+            <a href="#">Alumnos</a>
+        </li>
+        <li>
+            <a href="#">Admin</a>
+        </li>
+        <li>
+            <a href="#">Materia</a>
+        </li
+        <li>
+            <a href="#">Categorias</a>
+        </li>
+        <li>
+            <a href="#">Grupos</a>
+        </li>
+    </ul>
+</nav>
+    
+</body>
+</html>
