52aeace44a44ee45907b7e30031649d265afbc0f
bonnesbaby14 <bonnesbaby14@gmail.com>
2022-05-28 15:56:50 -0500

    cambio de base de datos

--- se_ceti.sql
+++ se_ceti.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 27-05-2022 a las 04:15:17
+-- Tiempo de generación: 28-05-2022 a las 22:56:16
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -31,9 +31,9 @@ CREATE TABLE `asignacion` (
   `id` int(11) NOT NULL,
   `idgrupo` int(11) DEFAULT NULL,
   `iduser` int(11) DEFAULT NULL,
-  `primer_parcial` double DEFAULT NULL,
-  `segundo_parcial` double DEFAULT NULL,
-  `tercer_parcial` double DEFAULT NULL,
+  `primer_parcial` double DEFAULT 0,
+  `segundo_parcial` double DEFAULT 0,
+  `tercer_parcial` double DEFAULT 0,
   `idmateria` int(11) DEFAULT NULL,
   `activo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -44,7 +44,12 @@ CREATE TABLE `asignacion` (
 
 INSERT INTO `asignacion` (`id`, `idgrupo`, `iduser`, `primer_parcial`, `segundo_parcial`, `tercer_parcial`, `idmateria`, `activo`) VALUES
 (1, 2, 2, NULL, NULL, NULL, 4, 0),
-(2, 2, 4, NULL, NULL, NULL, 3, 0);
+(2, 2, 4, NULL, NULL, NULL, 3, 0),
+(3, 3, 15, NULL, NULL, NULL, 6, 1),
+(4, 3, 1, 1, 2, 3, 6, 1),
+(5, 2, 1, 0, 0, 0, 3, 1),
+(6, 2, 10, 0, 0, 0, 3, 0),
+(7, 2, 10, 0, 0, 0, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -88,7 +93,8 @@ CREATE TABLE `grupo` (
 
 INSERT INTO `grupo` (`id`, `nombre`, `fecha`, `grado`, `activo`) VALUES
 (1, 'dmeossssaaaa', '2022-05-26 03:30:46', 2, 0),
-(2, 'numro ', '2022-05-27 07:43:33', 2, 1);
+(2, 'numro ', '2022-05-27 07:43:33', 2, 1),
+(3, 'test ', '2022-05-28 10:27:01', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -110,7 +116,8 @@ CREATE TABLE `materia` (
 INSERT INTO `materia` (`id`, `nombre`, `idcategoria`, `activo`) VALUES
 (3, 'MATEMATICAS', 2, 1),
 (4, 'DISEÑO INDUSTRIAL editado ', 2, 1),
-(5, 'español', 1, 0);
+(5, 'español', 1, 0),
+(6, 'test', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -148,7 +155,8 @@ INSERT INTO `user` (`id`, `user`, `password`, `nombre`, `apellidos`, `registro`,
 (11, 'admin edtitado', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin', 'admin', 'admin', NULL, 1),
 (12, 'sss', 'd41d8cd98f00b204e9800998ecf8427e', 'sss', '', '', 'admin', NULL, 0),
 (13, 'ddd', '1234', 'ddd', 'ddd', 'ddd', 'maestro', NULL, 1),
-(14, 'WS', '1234', 'WS', 'SS', 'SS', 'maestro', NULL, 0);
+(14, 'WS', '1234', 'WS', 'SS', 'SS', 'maestro', NULL, 0),
+(15, 'test', '1234', 'test', 'test', 'test', 'maestro', NULL, 1);
 
 --
 -- Índices para tablas volcadas
@@ -196,7 +204,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `asignacion`
 --
 ALTER TABLE `asignacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
@@ -208,19 +216,19 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `grupo`
 --
 ALTER TABLE `grupo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restricciones para tablas volcadas
