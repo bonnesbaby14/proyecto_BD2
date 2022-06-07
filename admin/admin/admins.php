a3f0529d0479df25e73873113465a7cd172dfe99
bonnesbaby14 <bonnesbaby14@gmail.com>
2022-06-06 19:46:46 -0500

    session de admin

--- admin/admin/admins.php
+++ admin/admin/admins.php
@@ -1,9 +1,9 @@
 <?php 
-/*session_start();
-if (!isset($_SESSION["ID"])) {
+session_start();
+if (!isset($_SESSION["ID"]) or $_SESSION["tipo"]=="admin") {
     header('Location: login.php');}
 
-*/
+
 
     include("../../config/db.php"); 
     
