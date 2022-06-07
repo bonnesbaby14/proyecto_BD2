6a25a23e1b9b493cac423745d3e1eeb1452890e0
bonnesbaby14 <bonnesbaby14@gmail.com>
2022-06-06 20:07:25 -0500

    dashboard admin sesison

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,11 +1,11 @@
 <?php
-/* session_start(); 
-if (!isset($_SESSION["ID"])) {
-    header('Location: login.php');
-} 
+
+session_start(); 
+if (!isset($_SESSION["ID"]) or $_SESSION["tipo"]!="admin") {
+    header('Location: ./../../login2.php ');}
 
 
-include("../config/db.php");  */ ?> 
+include("../config/db.php"); ?> 
 <!DOCTYPE html>
 <html lang="en">
 
